ab58032688c79b1f6401b4f20ee9327d08781857
Kamil Sudoł <kamilsudolorigin@gmail.com>
2022-07-17 00:27:57 +0200

    Prediction string improvement

--- app/gui/gui.py
+++ app/gui/gui.py
@@ -51,9 +51,9 @@ class MainGUI(QMainWindow):
         self.predict_button.setEnabled(bool(self.current_file))
 
     def render_text_label(self, prediction_text: str):
-        self.text_label.setMinimumWidth(WIDTH)
+        self.text_label.setMinimumWidth(self.width())
         self.text_label.setText(self.prediction_title.format(prediction_text))
-        self.text_label.move(self.width() / 3, self.height() - IMAGE_SPACE_HEIGHT * 0.66)
+        self.text_label.move(25, self.height() - IMAGE_SPACE_HEIGHT * 0.66)
 
     def resizeEvent(self, a0: QtGui.QResizeEvent):
         if self.pixmap_original:
@@ -89,8 +89,8 @@ class MainGUI(QMainWindow):
         self.current_prediction = self.prediction_processing
         self.emit_update()
         try:
-            country, possibility = run_module(self.current_file)[0]
-            self.current_prediction = f"{country} with {possibility}% confidence"
+            predictions = run_module(self.current_file)
+            self.current_prediction = ", ".join(f"{country} ({possibility}%)" for country, possibility in predictions)
         except IndexError:
             self.current_prediction = self.prediction_fail
         self.emit_update()
